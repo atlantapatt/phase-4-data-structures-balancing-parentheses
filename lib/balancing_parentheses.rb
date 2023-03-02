9984165ec333061d2aa76a2209e8cafa253a9fbd
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-03-01 16:11:31 -0800

    commit tree 16dcadb3ffe9ebdf8d44c364971aa4527f99c7d3

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,16 @@
 require_relative './stack'
 
 # your code here
+
+def balancing_parentheses(string)
+    stack = Stack.new
+    
+    string.chars.each do |i|
+        if stack.peek == '(' && i==')'
+            stack.pop
+        else
+            stack.push(i)
+        end
+    end
+    stack.size
+end
